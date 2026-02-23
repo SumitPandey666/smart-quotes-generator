07ac7141cf29dbf6a1d1a800b29a845ab94dd258
sumit <1920sumitpandey@gmail.com>
2026-02-23 23:22:38 +0530

    Quote Entity

---
+++ src/main/java/com/smartquotes/quoteservice/entity/Quote.java
@@ -0,0 +1,42 @@
+package com.smartquotes.quoteservice.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "quotes")
+@Getter
+@Setter @NoArgsConstructor
+public class Quote {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String text;
+
+    // ALWAYS use LAZY fetching for ToOne relationships to prevent N+1 issues
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "author_id", nullable = false)
+    private Author author;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "quote_tag_mapping",
+            joinColumns = @JoinColumn(name = "quote_id"),
+            inverseJoinColumns = @JoinColumn(name = "tag_id")
+    )
+    private Set<Tag> tags = new HashSet<>();
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+}
