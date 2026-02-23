d01b6fa97583b5fefc237c1f3c8cac0ffc684f10
sumit <1920sumitpandey@gmail.com>
2026-02-23 23:20:44 +0530

    Author and Tag entity

---
+++ src/main/java/com/smartquotes/quoteservice/entity/Author.java
@@ -0,0 +1,32 @@
+package com.smartquotes.quoteservice.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "authors")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(columnDefinition = "TEXT")
+    private String bio;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+}
